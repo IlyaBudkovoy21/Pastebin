182560cf6c74a2ab534f7c6c2be9d26f3e44f16c
Ilya <ilyabudkovoy@yandex.ru>
2024-06-04 18:46:24 +0300

    Создал новую модель для регистрации пользователя по email

--- Pastebin/users/views.py
+++ Pastebin/users/views.py
@@ -9,10 +9,14 @@ def login_user(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        print(user)
         if user is not None:
             login(request, user)
             return redirect(reverse('users:personal_account'))
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+            form = UserLoginForm()
+            form.username = request.POST['username']
+            return render(request, "users/registration/login.html", {'form': form})
     form = UserLoginForm()
     return render(request, "users/registration/login.html", {'form': form})
 
@@ -26,11 +30,13 @@ def registration(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
             password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
+            user = authenticate(username=email, password=password)
             login(request, user)
             messages.success(request, 'Registration successful')
+            user.email_user('Confirmation of registration',
+                            'Please confirm your email address for registration on Pastebin. If you have not registered, please ignore this message.')
             return redirect(reverse('users:registration_success'))
         else:
             return render(request, 'users/registration/registration.html', {'form': form})
@@ -47,8 +53,3 @@ def personal_account(request):
 def logout_user(request):
     logout(request)
     return render(request, 'users/registration/logout.html')
-
-
-def update_user(request):
-    if request.method == 'POST':
-        form =
